55e003fa0cf6e2ecf34846f2f1b2cd10de618049
陈维锐 <17876574358@163.com>
2023-04-06 09:15:32 +0800

    feat: add linear list definition

---
+++ 数据结构/数据结构/源码/第二章 线性表/2.2 linear list sequence storage.c
@@ -0,0 +1,6 @@
+#define MaxSize 50              // 定义线性表的最大长度
+typedef struct{
+    ElemType data[MaxSize];     // 顺序表中的元素
+    int length;                 // 顺序表当前的长度
+}SqList;                        // 顺序表的类型定义
+
