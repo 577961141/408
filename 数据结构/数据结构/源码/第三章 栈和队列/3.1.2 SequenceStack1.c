3e13f0bfc81e102d0a85f3b55b4de402d5fa27f5
陈维锐 <17876574358@163.com>
2023-05-16 09:05:15 +0800

    feat：conduct Link Stack

---
+++ 数据结构/数据结构/源码/第三章 栈和队列/3.1.2 SequenceStack1.c
@@ -0,0 +1,73 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "math.h"
+#include "time.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define MAXSIZE 20
+
+typedef int Status;
+typedef int SElemType;
+
+typedef  struct {
+     SElemType data[MAXSIZE];
+     int top;
+}SequenceStack;
+
+Status InitStack(SequenceStack *s) {
+     s->top = -1;
+     return OK;
+}
+
+Status ClearStack(SequenceStack *s) {
+     s->top = -1;
+     return OK;
+}
+
+Status StackEmpty(SequenceStack s) {
+     if (s.top == -1) {
+         return TRUE;
+     }
+
+     return FALSE;
+}
+
+Status StackLength(SequenceStack s) {
+     return s.top + 1;
+     // return s.top;
+}
+
+Status Push(SequenceStack *s, SElemType e) {
+     if (s->top == MAXSIZE-1){
+      // if (s->top == MAXSIZE-1){
+         return ERROR;
+     }
+
+     s->data[++s->top] = e;
+     // s->data[s->top++] = e
+
+     return OK;
+}
+
+Status Pop(SequenceStack *s, SElemType *e) {
+     if (s->top == -1) {
+         return ERROR;
+     }
+
+     *e = s->data[s->top--];
+     return OK;
+}
+
+Status GetTop(SequenceStack s, SElemType *e) {
+     if (s.top == -1) {
+         return ERROR;
+     }
+
+     *e = s.data[s.top];
+     // *e = s->data[s->top-1];
+     return OK;
+}
