13c13e3dc020e53a4c58ae22553a0ea651a8b469
陈维锐 <17876574358@163.com>
2023-05-25 09:01:08 +0800

    feat：add stack application

--- 数据结构/数据结构/源码/第三章 栈和队列/LinkQueue1.c
+++ 数据结构/数据结构/源码/第三章 栈和队列/LinkQueue1.c
@@ -3,100 +3,120 @@
 #include "math.h"
 #include "time.h"
 
+
 #define OK 1
 #define ERROR 0
 #define TRUE 1
 #define FALSE 0
+
 typedef int Status;
-typedef int QElemType;
-typedef struct QNode
-{
-     QElemType data;
-     struct QNode *next;
-}QNode, *QueuePtr;
-typedef struct
-{
-     QueuePtr front, rear;
+typedef int QElement;
+
+typedef struct QNode{
+    QElement data;
+    struct QNode *next;
+}QNode, *LinkQueuePtr;
+
+typedef struct {
+    LinkQueuePtr front,rear;
 }LinkQueue;
-Status InitQueue(LinkQueue *Q) {
-     Q->front = Q->rear = (QueuePtr)malloc(sizeof(QNode));
+
+Status InitQueue(LinkQueue *Q)
+{
+     // 带头结点也是这样，不带头结点也这样
+    Q->front = Q->rear = (LinkQueuePtr)malloc(sizeof(QNode));
+
      if (!Q->front) {
          return ERROR;
      }
+
      Q->front->next = NULL;
      return OK;
 }
-Status DestroyQueue(LinkQueue *Q)
-{
+
+Status DestroyQueue(LinkQueue *Q) {
+     // 带头结点也是这样，不带头结点也这样
      while (Q->front) {
          Q->rear = Q->front->next;
          free(Q->front);
          Q->front = Q->rear;
      }
+
      return OK;
 }
-Status clearQueue(LinkQueue *Q)
-{
-     QueuePtr p,q;
+
+Status ClearQueue(LinkQueue *Q) {
+     // 带头结点
+     LinkQueuePtr p,q;
      Q->rear = Q->front;
      p = Q->front->next;
      Q->front->next = NULL;
+
      while(p) {
          q = p;
-         p = p->next;
+         p = q->next;
          free(q);
      }
+
      return OK;
 }
+
 Status QueueEmpty(LinkQueue Q) {
+     // 带头结点
      if (Q.front == Q.rear) {
          return TRUE;
      } else {
          return FALSE;
      }
+
+     // 不带头结点
+      if (Q.front == NULL) {
+          return TRUE;
+      } else {
+          return FALSE;
+      }
 }
-Status EnQueue(LinkQueue *Q, QElemType e)
-{
-     QueuePtr s = (QueuePtr)malloc(sizeof(QNode));
+
+ int QueueLength(LinkQueue Q) {
+     int i = 0;
+     LinkQueuePtr p;
+     p = Q.front;
+     while(Q.rear != p) {
+         i++;
+         p = p->next;
+     }
+
+     return i;
+}
+
+Status EnQueue(LinkQueue *Q, QElement e) {
+     LinkQueuePtr s = (LinkQueuePtr)malloc(sizeof(QNode));
      if (!s) {
          return ERROR;
      }
+
      s->data = e;
      s->next = NULL;
      Q->rear->next = s;
      Q->rear = s;
      return OK;
 }
-Status DeQueue(LinkQueue *Q, QElemType *e) {
-     QueuePtr s;
-     if (Q->front == Q->rear) {
+
+Status DeQueue(LinkQueue *Q, QElement *e) {
+     if (Q->front == Q->rear) { // 空的
          return ERROR;
      }
-     s = Q->front->next;
-     *e = s->data;
-     Q->front->next = s->next;
-     if (s == Q->rear) {
+
+      LinkQueuePtr p;
+
+     p = Q->front->next;
+     *e = p->data;
+     Q->front->next = p->next;
+
+     if (p == Q->rear) {
          Q->rear = Q->front;
      }
-     free(s);
-     return OK;
-}
- int QueueLength(LinkQueue Q)
-{
-     QueuePtr p;
-     p = Q.front->next;
-     int count = 0;
-     while (p) {
-         count++;
-         p = p->next;
-     }
-     return count;
-}
-Status GetHead(LinkQueue Q, QElemType *e)
-{
-     if (Q.front == Q.rear) {
-         return ERROR;
-     }
-     *e = Q.front->next->data;
+
+     free(p);
      return OK;
 }
